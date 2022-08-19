18fcdc4ef72a2f6fcea4a418de700b1c9a639a31
LucasFsc <lucas.fscr@gmail.com>
2022-08-19 15:01:31 -0300

    add swagger n class vaidator

--- apps/publisher/src/app.controller.ts
+++ apps/publisher/src/app.controller.ts
@@ -1,18 +1,22 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { ApiOkResponse, ApiTags } from '@nestjs/swagger';
 import { AppService } from './app.service';
 import { MultiplyByTwoDto } from './dto/multiply-by-two.dto';
 import { SumDto } from './dto/sum.dto';
 
+@ApiTags('math')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOkResponse()
   @HttpCode(HttpStatus.OK)
   @Post('/sum')
   async sum(@Body() { data }: SumDto) {
     return this.appService.sum(data);
   }
 
+  @ApiOkResponse()
   @HttpCode(HttpStatus.OK)
   @Post('/multiply_by_two')
   async multiplyByTwo(@Body() { data }: MultiplyByTwoDto) {
